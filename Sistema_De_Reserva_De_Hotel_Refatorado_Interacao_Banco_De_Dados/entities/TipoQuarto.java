6bf42e6577b5adb8f468a58d14d3f852d8fae8a2
Felberque <guilhermefelberque.2009@gmail.com>
2026-07-03 15:52:39 -0300

    Delete Sistema_De_Reserva_De_Hotel_Refatorado_Interacao_Banco_De_Dados directory

--- Sistema_De_Reserva_De_Hotel_Refatorado_Interacao_Banco_De_Dados/entities/TipoQuarto.java
+++
@@ -1,7 +0,0 @@
-package JDBC.Sistema_De_Reserva_De_Hotel_Refatorado_Interacao_Banco_De_Dados.entities;
-
-public enum TipoQuarto {
-    SIMPLES,
-    DUPLO,
-    SUITE
-}
--- Sistema_De_Reserva_De_Hotel_Refatorado_Interacao_Banco_De_Dados/enums/Pagamento.java
+++
@@ -1,7 +0,0 @@
-package JDBC_1_Pratica.Exercicio_6_Sistema_de_Reserva_de_Hotel.enums;
-
-public enum Pagamento {
-    PIX,
-    Dinheiro,
-    Cartao
-}
